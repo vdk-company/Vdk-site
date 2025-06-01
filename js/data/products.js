e8868be3b0e81daf324833a303174b670e6282c0
vdk-company <eclat154@gmail.com>
2025-06-01 20:43:15 +0300

    Update products.js

--- js/data/products.js
+++ js/data/products.js
@@ -1,159 +1,156 @@
 const products = [
-  // Азот
   {
-    id: "azot1",
-    category: "Азот",
+    id: "azot-10",
     name: "Азот",
-    volumeOptions: [
-      { volume: "10 л", price: 10000 },
-      { volume: "40 л", price: 22000 },
+    type: "Газ",
+    variants: [
+      { volume: "10 л", price: 10000 }
     ],
-    image: "images/azot.png",
+    image: "images/azot.jpg"
   },
   {
-    id: "azotOch",
-    category: "Азот",
-    name: "Азот ОЧ",
-    volumeOptions: [{ volume: "40 л", price: 22000 }],
-    image: "images/azot_och.png",
-  },
-
-  // Аргон
-  {
-    id: "argon1",
-    category: "Аргон",
-    name: "Аргон (40л/6.3 м3)",
-    volumeOptions: [{ volume: "40 л", price: 15000 }],
-    image: "images/argon_40l.png",
+    id: "azot-och",
+    name: "Азот особ. чистоты",
+    type: "Газ",
+    variants: [
+      { volume: "10 л", price: 22000 }
+    ],
+    image: "images/azot-och.jpg"
   },
   {
-    id: "argonSpecial",
-    category: "Аргон",
-    name: "Аргон Особ. Чистоты",
-    volumeOptions: [{ volume: "40 л", price: 22000 }],
-    image: "images/argon_special.png",
+    id: "argon-40",
+    name: "Аргон (40л / 6.3 м3)",
+    type: "Газ",
+    variants: [
+      { volume: "40 л", price: 15000 }
+    ],
+    image: "images/argon-40.jpg"
   },
   {
-    id: "argon10",
-    category: "Аргон",
-    name: "Аргон 10л (газ)",
-    volumeOptions: [{ volume: "10 л", price: 6000 }],
-    image: "images/argon_10l.png",
+    id: "argon-och",
+    name: "Аргон особ. чистоты",
+    type: "Газ",
+    variants: [
+      { volume: "40 л", price: 22000 }
+    ],
+    image: "images/argon-och.jpg"
   },
-
-  // Ацетилен
   {
-    id: "acetylene",
-    category: "Ацетилен",
+    id: "argon-10",
+    name: "Аргон (10л)",
+    type: "Газ",
+    variants: [
+      { volume: "10 л", price: 6000 }
+    ],
+    image: "images/argon-10.jpg"
+  },
+  {
+    id: "acetylen",
     name: "Ацетилен",
-    volumeOptions: [{ volume: "40 л", price: 47900 }],
-    image: "images/acetylene.png",
-  },
-
-  // Кислород
-  {
-    id: "oxygen",
-    category: "Кислород",
-    name: "Кислород",
-    volumeOptions: [
-      { volume: "10 л", price: 3000 },
-      { volume: "40 л", price: 3500 },
+    type: "Газ",
+    variants: [
+      { volume: "40 л", price: 47900 }
     ],
-    image: "images/oxygen.png",
-  },
-
-  // Пропан
-  {
-    id: "propaneSmall",
-    category: "Пропан",
-    name: "Пропан мал.",
-    volumeOptions: [{ volume: "10 кг", price: 2500 }],
-    extraPrices: { tara: 9000, ventil: 4000 },
-    image: "images/propane_small.png",
+    image: "images/acetylen.jpg"
   },
   {
-    id: "propaneLarge",
-    category: "Пропан",
-    name: "Пропан бол. (20кг)",
-    volumeOptions: [{ volume: "20 кг", price: 5000 }],
-    extraPrices: { tara: 26000 },
-    image: "images/propane_large.png",
+    id: "oxygen-3000",
+    name: "Кислород",
+    type: "Газ",
+    variants: [
+      { volume: "10 л", price: 3000 },
+      { volume: "40 л", price: 3500 }
+    ],
+    image: "images/oxygen.jpg"
   },
-
-  // Углекислота
   {
-    id: "co2_40",
-    category: "Углекислота",
+    id: "co2-40",
     name: "Углекислота (40 л)",
-    volumeOptions: [{ volume: "40 л", price: 25000 }],
-    image: "images/co2_40l.png",
+    type: "Газ",
+    variants: [
+      { volume: "40 л", price: 25000 }
+    ],
+    image: "images/co2-40.jpg"
   },
   {
-    id: "co2_20",
-    category: "Углекислота",
+    id: "co2-20",
     name: "Углекислота (20 л)",
-    volumeOptions: [{ volume: "20 л", price: 23000 }],
-    image: "images/co2_20l.png",
+    type: "Газ",
+    variants: [
+      { volume: "20 л", price: 23000 }
+    ],
+    image: "images/co2-20.jpg"
   },
   {
-    id: "co2_10",
-    category: "Углекислота",
+    id: "co2-10",
     name: "Углекислота (10 л)",
-    volumeOptions: [{ volume: "10 л", price: 11000 }],
-    image: "images/co2_10l.png",
+    type: "Газ",
+    variants: [
+      { volume: "10 л", price: 11000 }
+    ],
+    image: "images/co2-10.jpg"
   },
   {
-    id: "co2_5",
-    category: "Углекислота",
+    id: "co2-5",
     name: "Углекислота (5 л)",
-    volumeOptions: [{ volume: "5 л", price: 6000 }],
-    image: "images/co2_5l.png",
-  },
-
-  // Тара
-  {
-    id: "tara_10l",
-    category: "Тара",
-    name: "Тара (любой газ, кроме ацетилена) 10 л",
-    volumeOptions: [{ volume: "10 л", price: 57700 }],
-    image: "images/tara_10l.png",
+    type: "Газ",
+    variants: [
+      { volume: "5 л", price: 6000 }
+    ],
+    image: "images/co2-5.jpg"
   },
   {
-    id: "tara_40l_pereatt",
-    category: "Тара",
-    name: "Тара (40 л) переаттестованный",
-    volumeOptions: [{ volume: "40 л", price: 62000 }],
-    image: "images/tara_40l_pereatt.png",
+    id: "tara-10",
+    name: "Тара (10 л, новый)",
+    type: "Тара",
+    variants: [
+      { volume: "10 л", price: 57700 }
+    ],
+    image: "images/tara-10.jpg"
   },
   {
-    id: "tara_40l_new",
-    category: "Тара",
-    name: "Тара (40 л) новый",
-    volumeOptions: [{ volume: "40 л", price: 107900 }],
-    image: "images/tara_40l_new.png",
+    id: "tara-40-recert",
+    name: "Тара (40 л, переаттестованная)",
+    type: "Тара",
+    variants: [
+      { volume: "40 л", price: 62000 }
+    ],
+    image: "images/tara-40.jpg"
   },
-
-  // Пропан дополнительные
   {
-    id: "propane_small_10kg",
-    category: "Пропан",
-    name: "Пропан мал. (10 кг)",
-    volumeOptions: [{ volume: "10 кг", price: 9000 }],
-    image: "images/propane_small_10kg.png",
+    id: "tara-40-new",
+    name: "Тара (40 л, новая)",
+    type: "Тара",
+    variants: [
+      { volume: "40 л", price: 107900 }
+    ],
+    image: "images/tara-40-new.jpg"
+  },
+  {
+    id: "propane-small",
+    name: "Пропан малый (10 кг)",
+    type: "Газ",
+    variants: [
+      { volume: "10 кг", price: 9000 }
+    ],
+    image: "images/propane-small.jpg"
+  },
+  {
+    id: "propane-big",
+    name: "Пропан большой (20 кг)",
+    type: "Газ",
+    variants: [
+      { volume: "20 кг", price: 26000 }
+    ],
+    image: "images/propane-big.jpg"
   },
   {
     id: "ventil",
-    category: "Пропан",
     name: "Вентиль",
-    volumeOptions: [{ volume: "шт", price: 4000 }],
-    image: "images/ventil.png",
-  },
-  {
-    id: "propane_big_20kg",
-    category: "Пропан",
-    name: "Пропан бол. (20 кг)",
-    volumeOptions: [{ volume: "20 кг", price: 26000 }],
-    image: "images/propane_big_20kg.png",
+    type: "Оборудование",
+    variants: [
+      { volume: "-", price: 4000 }
+    ],
+    image: "images/ventil.jpg"
   }
-  // Добавь остальные товары из ссылок по такому же шаблону
 ];
