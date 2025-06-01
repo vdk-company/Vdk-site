0d883998ca578ab2bf210039cdf059b1ade54f18
vdk-company <eclat154@gmail.com>
2025-06-01 21:54:55 +0300

    Delete VDK_final_site directory

--- VDK_final_site/data/products.js
+++
@@ -1,5 +0,0 @@
-const productData = [
-  { name: "Азот", volume: "40 л", price: 10000 },
-  { name: "Аргон", volume: "10 л", price: 8000 },
-  { name: "Кислород", volume: "20 л", price: 7000 }
-];
--- VDK_final_site/scripts/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const productContainer = document.getElementById("products");
-  productData.forEach(product => {
-    const div = document.createElement("div");
-    div.innerHTML = `<h3>${product.name}</h3><p>${product.volume}</p><p>${product.price} тг</p>`;
-    productContainer.appendChild(div);
-  });
-});
